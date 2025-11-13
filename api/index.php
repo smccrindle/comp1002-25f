e33893a81aa8b8225a8b707fcc52ac666533354f
Scott McCrindle <iainmccrindle@gmail.com>
2025-11-13 17:13:49 -0500

    Single routing point

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
-    phpinfo();
+    $uri = $_SERVER['REQUEST_URI'];
+    // You'd typically strip the query string and base directory here
+    $path = trim(parse_url($uri, PHP_URL_PATH), '/');
+
+    // Example: Map 'users/profile' to 'pages/users/profile.php'
+    $target_file = 'pages/' . $path . '.php';
+
+    echo('<h1>'.$target_file.'</h1>');
+
+    /*
+    if (file_exists($target_file)) {
+        // This executes the code in pages/users/profile.php
+        require $target_file;
+    } else {
+        // Handle 404
+        http_response_code(404);
+        echo "404 Page Not Found";
+    }
+    */
 ?>
